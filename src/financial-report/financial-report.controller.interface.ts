58998f5ab0f8311b2a2a3e50c374f54ccf6e9e3a
Dmitriy Korobchenko <deankorobchenko@scrile.com>
2022-02-18 22:06:44 +0700

    move controller to a folder

--- src/financial-report/financial-report.controller.interface.ts
+++
@@ -1,8 +0,0 @@
-import { AppRoute } from "../types/route.types";
-import { FinancialReport } from "./types";
-
-export default interface IFinancialReportController {
-  // getAll(): Promise<FinancialReport[]>;
-  getAll(): Promise<string[]>;
-  routes: AppRoute[];
-}

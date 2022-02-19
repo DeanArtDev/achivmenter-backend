725da49c1a446aeb4f4042d63a46e3e1ca9da5c6
Dmitriy Korobchenko <deankorobchenko@scrile.com>
2022-02-20 01:30:27 +0700

    update repository

--- src/financial-report/repository/financial-report.repository.interface.ts
+++ src/financial-report/repository/financial-report.repository.interface.ts
@@ -1,7 +1,7 @@
 import FinancialReport from "../entity/financial-report.entity";
-import { FinancialReportModel } from ".prisma/client";
+import { FinancialPeriodModelComplete } from "../types";
 
 export default interface IFinancialReportRepository {
-  create(report: FinancialReport): Promise<FinancialReportModel>;
-  getAll(): Promise<FinancialReportModel[]>;
+  create(report: FinancialReport): Promise<FinancialPeriodModelComplete>;
+  getAll(): Promise<FinancialPeriodModelComplete[]>;
 }
--- src/financial-report/repository/financial-report.repository.ts
+++ src/financial-report/repository/financial-report.repository.ts
@@ -1,10 +1,10 @@
-import { FinancialReportModel } from "@prisma/client";
 import { injectable, inject } from "inversify";
 import { dependenciesType } from "../../dependencies.types";
 import { ILogger } from "../../logger";
 import IFinancialReportRepository from "./financial-report.repository.interface";
 import { IDataBaseService } from "../../database";
 import FinancialReport from "../entity/financial-report.entity";
+import { FinancialPeriodModelComplete } from "../types";
 
 @injectable()
 export default class FinancialReportRepository implements IFinancialReportRepository {
@@ -13,14 +13,17 @@ export default class FinancialReportRepository implements IFinancialReportReposi
     @inject(dependenciesType.IDataBaseService) private readonly db: IDataBaseService,
   ) {}
 
-  public async create(report: FinancialReport): Promise<FinancialReportModel> {
+  public async create(report: FinancialReport): Promise<FinancialPeriodModelComplete> {
+    console.log(report);
     return await this.db.client.financialReportModel.create({
       data: { parts: { create: report.parts }, period: { create: report.period } },
       include: { parts: true, period: true },
     });
   }
 
-  public async getAll(): Promise<FinancialReportModel[]> {
-    return await this.db.client.financialReportModel.findMany({ include: { parts: true, period: true } });
+  public async getAll(): Promise<FinancialPeriodModelComplete[]> {
+    return await this.db.client.financialReportModel.findMany({
+      include: { parts: true, period: true },
+    });
   }
 }

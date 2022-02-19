ad6c168c0eb99e7c7b55308943083d9a93f74e58
Dmitriy Korobchenko <deankorobchenko@scrile.com>
2022-02-20 01:31:51 +0700

    add validation schemas

--- src/financial-report/financial-report.validation.schema.ts
+++ src/financial-report/financial-report.validation.schema.ts
@@ -1,5 +1,4 @@
 const FinancialPartSchema = {
-  id: { type: "string" },
   income: { type: "number" },
   common: { type: "number" },
   piggyBank: { type: "number" },
@@ -8,13 +7,8 @@ const FinancialPartSchema = {
 
 const FinancialPeriodSchema = {
   month: { type: "number" },
+  year: { type: "number" },
   partCount: { type: "number" },
-};
-
-export const FinancialPeriodResponseSchema = {
-  type: "object",
-  required: ["id", "period", "parts"],
-  properties: getFinancialReportSchema(),
 };
 
 function getFinancialReportSchema(
@@ -22,15 +16,14 @@ function getFinancialReportSchema(
   periodRequired?: FinancialPeriodSchemaTypes[],
 ) {
   return {
-    id: { type: "string" },
     parts: {
       type: "array",
-      required: partRequired ?? ["id", "income", "common", "piggyBank", "free"],
+      required: partRequired ?? ["income", "common", "piggyBank", "free"],
       item: FinancialPartSchema,
     },
     period: {
       type: "object",
-      required: periodRequired ?? ["month", "partCount"],
+      required: periodRequired ?? ["month", "year", "partCount"],
       properties: FinancialPeriodSchema,
     },
   };
@@ -38,3 +31,43 @@ function getFinancialReportSchema(
 
 type FinancialPartSchemaTypes = "id" | "income" | "common" | "piggyBank" | "free";
 type FinancialPeriodSchemaTypes = "month" | "partCount";
+
+export const FinancialPeriodResponseSchema = {
+  type: "object",
+  required: ["id", "period", "parts"],
+  properties: {
+    id: { type: "string" },
+    ...getFinancialReportSchema(["id", "income", "common", "piggyBank", "free"]),
+    parts: {
+      ...getFinancialReportSchema(["id", "income", "common", "piggyBank", "free"]).parts,
+      item: {
+        id: { type: "string" },
+        ...FinancialPartSchema,
+      },
+    },
+  },
+};
+
+export const FinancialPeriodRequestSchema = {
+  type: "object",
+  required: ["period", "parts"],
+  properties: getFinancialReportSchema(),
+};
+
+export const validationSchemaOfCreate = {
+  tags: ["Financial report"],
+  description: "Create a financial report",
+  body: FinancialPeriodRequestSchema,
+  response: { 200: FinancialPeriodResponseSchema },
+};
+
+export const validationSchemaOfGetAll = {
+  tags: ["Financial report"],
+  description: "Get all financial reports",
+  response: {
+    200: {
+      type: "array",
+      item: FinancialPeriodResponseSchema,
+    },
+  },
+};
--- src/financial-report/index.ts
+++
@@ -1,4 +0,0 @@
-import FinancialReportController from "./controller/financial-report.controller";
-import IFinancialReportController from "./controller/financial-report.controller.interface";
-
-export { IFinancialReportController, FinancialReportController };

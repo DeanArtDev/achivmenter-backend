38ac63caa977ff4094bebe7d79bcaafd768abdf2
Dmitriy Korobchenko <deankorobchenko@scrile.com>
2022-02-20 01:29:50 +0700

    update entity

--- src/financial-report/entity/financial-report.entity.ts
+++ src/financial-report/entity/financial-report.entity.ts
@@ -5,10 +5,7 @@ export default class FinancialReport {
   public readonly period!: FinancialPeriod;
   public readonly parts!: FinancialPart[];
 
-  constructor(
-    period: { month: number; year: number; partCount: number },
-    parts: { income: number; common: number; piggyBank: number; free: number }[],
-  ) {
+  constructor(period: FinancialPeriod, parts: FinancialPart[]) {
     this.period = new FinancialPeriod(period.month, period.partCount, period.year);
     this.parts = parts.map((p) => new FinancialPart(p.income, p.common, p.piggyBank, p.free));
   }

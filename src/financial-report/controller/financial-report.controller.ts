3b3197402547d5b3c955966fd3c9059f34aaa134
Dmitriy Korobchenko <deankorobchenko@scrile.com>
2022-02-20 01:29:28 +0700

    update financial controller

--- src/financial-report/controller/financial-report.controller.ts
+++ src/financial-report/controller/financial-report.controller.ts
@@ -1,28 +1,32 @@
 import { inject, injectable } from "inversify";
-import { FinancialReportModel } from "@prisma/client";
 import IFinancialReportController from "./financial-report.controller.interface";
 import { dependenciesType } from "../../dependencies.types";
 import { ILogger } from "../../logger";
 import { AppRoute } from "../../types/route.types";
 import { IFinancialReportService } from "../service";
 import { FastifyReply, FastifyRequest } from "fastify";
+import { FinancialReportResponseDTO } from "../dto/financial-report.dto";
+import { validationSchemaOfCreate, validationSchemaOfGetAll } from "../financial-report.validation.schema";
+import { FinancialPeriodModelComplete } from "../types";
 import "reflect-metadata";
 
 @injectable()
 export default class FinancialReportController implements IFinancialReportController {
   //todo: add the validation schema
-  private readonly url = "/financial-report"
+  private readonly url = "/financial-report";
   public readonly routes: AppRoute[] = [
     {
       url: this.url,
       method: "GET",
-      handler: this.onGetHandler.bind(this),
+      schema: validationSchemaOfGetAll,
+      handler: this.onGetAllHandler.bind(this),
     },
     {
       url: this.url,
+      schema: validationSchemaOfCreate,
       method: "POST",
-      handler: this.onPostHandler.bind(this),
-    }
+      handler: this.onCreateHandler.bind(this),
+    },
   ];
 
   constructor(
@@ -30,15 +34,37 @@ export default class FinancialReportController implements IFinancialReportContro
     @inject(dependenciesType.IFinancialReportService) private readonly financialReportService: IFinancialReportService,
   ) {}
 
-  private async onGetHandler(_:FastifyRequest, replay: FastifyReply): Promise<void> {
+  private async onGetAllHandler(_: FastifyRequest, replay: FastifyReply): Promise<void> {
     const reports = await this.financialReportService.getAll();
-    replay.code(200).send(reports);
+    replay.code(200).send(reports.map(this.reportAdapter));
   }
 
-  private async onPostHandler(request:FastifyRequest, replay: FastifyReply): Promise<void> {
-    console.log(request.body);
-    
-    const reports = await this.financialReportService.createReport(request.body)
-    // replay.code(200).send(reports);
+  private async onCreateHandler(
+    request: FastifyRequest<{ Body: FinancialReportResponseDTO }>,
+    replay: FastifyReply,
+  ): Promise<void> {
+    const report = await this.financialReportService.createReport({
+      parts: request.body.parts,
+      period: request.body.period,
+    });
+    replay.send(this.reportAdapter(report));
+  }
+
+  private reportAdapter({ id, period, parts }: FinancialPeriodModelComplete): FinancialReportResponseDTO {
+    return {
+      id: String(id),
+      period: {
+        month: period.month,
+        year: period.year,
+        partCount: period.partCount,
+      },
+      parts: parts.map((p) => ({
+        id: String(p.id),
+        income: p.income,
+        common: p.common,
+        piggyBank: p.piggyBank,
+        free: p.free,
+      })),
+    };
   }
 }
---
+++ src/financial-report/controller/index.ts
@@ -0,0 +1,4 @@
+import FinancialReportController from "./financial-report.controller";
+import IFinancialReportController from "./financial-report.controller.interface";
+
+export { IFinancialReportController, FinancialReportController };

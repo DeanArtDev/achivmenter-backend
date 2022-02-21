b0ae7a3169ef03973ffd409dbd17e0938c54b43e
Dmitriy Korobchenko <deankorobchenko@scrile.com>
2022-02-21 13:48:29 +0700

    add financial controller

--- src/financial-report/controller/financial-report.controller.ts
+++ src/financial-report/controller/financial-report.controller.ts
@@ -5,7 +5,7 @@ import { AppRoute } from "../../types/route.types";
 import { IFinancialReportService } from "../service";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { FinancialReportResponseDTO } from "../dto/financial-report.dto";
-import { FinancialPeriodModelComplete } from "../types";
+import { FinancialReportModelComplete } from "../types";
 import { dependenciesType } from "../../dependencies.types";
 import { validationSchemaOfCreate, validationSchemaOfGetAll } from "../financial-report.validation.schema";
 import { BaseController } from "../../common/base.controller";
@@ -29,6 +29,12 @@ export default class FinancialReportController extends BaseController implements
       handler: this.onCreateHandler.bind(this),
     },
     {
+      url: this.url,
+      // schema: validationSchemaOfCreate,
+      method: "PUT",
+      handler: this.onUpdateHandler.bind(this),
+    },
+    {
       url: this.url + "/:id",
       // schema: validationSchemaOfDelete,
       method: "DELETE",
@@ -46,36 +52,48 @@ export default class FinancialReportController extends BaseController implements
 
   private async onGetAllHandler(_: FastifyRequest, replay: FastifyReply): Promise<void> {
     const reports = await this.financialReportService.getAll();
-    this.ok<FinancialPeriodModelComplete[]>(replay, reports.map(this.reportAdapter));
+    this.ok<FinancialReportModelComplete[]>(replay, reports.map(this.reportAdapter));
   }
 
   private async onCreateHandler(
     request: FastifyRequest<{ Body: FinancialReportResponseDTO }>,
     replay: FastifyReply,
   ): Promise<void> {
-    const report = await this.financialReportService.createReport({
+    const report = await this.financialReportService.create({
       parts: request.body.parts,
       period: request.body.period,
     });
     this.create(replay).send(this.reportAdapter(report));
   }
 
+  // todo: обязательно id при update
+  private async onUpdateHandler(
+    request: FastifyRequest<{ Body: FinancialReportResponseDTO }>,
+    replay: FastifyReply,
+  ): Promise<void> {
+    const { id, parts, period } = request.body;
+    const updatedReport = await this.financialReportService.update({ id, parts, period });
+    this.ok<FinancialReportModelComplete>(replay, updatedReport);
+  }
+
   private async onDeleteHandler(
     { params }: FastifyRequest<{ Params: { id: string } }>,
     replay: FastifyReply,
   ): Promise<void> {
-    const isDeleted = await this.financialReportService.deleteReport(params.id);
+    const isDeleted = await this.financialReportService.delete(params.id);
     this.ok<boolean>(replay, isDeleted);
   }
 
-  private reportAdapter({ id, period, parts }: FinancialPeriodModelComplete): any {
+  private reportAdapter({ id, period: newPeriod, parts }: FinancialReportModelComplete): any {
     return {
       id: String(id),
-      period: {
-        month: period.month,
-        year: period.year,
-        partCount: period.partCount,
-      },
+      period: newPeriod
+        ? {
+            month: newPeriod.month,
+            year: newPeriod.year,
+            partCount: newPeriod.partCount,
+          }
+        : undefined,
       parts: parts.map((p) => ({
         id: String(p.id),
         income: p.income,

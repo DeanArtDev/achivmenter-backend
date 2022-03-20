71734036fa54c2eeb0dae217bbc5ff8238891b45
Dmitriy Korobchenko <deankorobchenko@scrile.com>
2022-03-20 14:01:24 +0700

    update docker .env configs (#6)

--- runpg.sh
+++ runpg.sh
@@ -1,7 +1,7 @@
 ROOT_DIR=$(pwd);
 
 docker start local-db-postgres ||
-docker run -d --rm --name local-db-postgres -p 5455:5432 \
+docker run -d --rm --name local-db-postgres -p 5432:5432 \
  -e POSTGRES_DB=db_achivmenter_local_dev \
  -e POSTGRES_USER=db_admin \
  -e POSTGRES_PASSWORD=local_bd \

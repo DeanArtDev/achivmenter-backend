ad9760e1601da77930231e32a5bd9bdadef7e7f5
Dmitriy Korobchenko <deankorobchenko@scrile.com>
2022-03-25 16:51:07 +0700

    add middleware

--- src/middlewares/auth-guard.middleware.ts
+++ src/middlewares/auth-guard.middleware.ts
@@ -4,7 +4,7 @@ import { HookHandlerDoneFunction } from "fastify/types/hooks";
 
 export default class AuthGuardMiddleware implements IMiddleware {
   execute(request: FastifyRequest, reply: FastifyReply, done: HookHandlerDoneFunction) {
-    console.log("GUARD");
-    done()
+    if (reply.context.config.authUser) return done();
+    reply.code(401).send({ message: "Unauthorised" });
   }
 }

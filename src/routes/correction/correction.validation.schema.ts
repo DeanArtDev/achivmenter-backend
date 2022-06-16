28fc686e07aa7495752a1ce225fc76da0a23d03d
Dmitriy Korobchenko <deankorobchenko@scrile.com>
2022-06-16 17:23:22 +0700

    refactoring order of validation schema

--- src/routes/correction/correction.validation.schema.ts
+++ src/routes/correction/correction.validation.schema.ts
@@ -3,10 +3,23 @@ const CorrectionSchema = {
   amount: { type: "number" },
 };
 
+const CorrectionResponseSchema = {
+  type: "object",
+  required: ["id", "name", "amount"],
+  properties: { id: { type: "string" }, ...CorrectionSchema },
+};
+
 const CorrectionCreateSchema = {
   type: "object",
   required: ["name", "amount", "financialPartId"],
   properties: { financialPartId: { type: "string" }, ...CorrectionSchema },
+};
+
+export const validationSchemaCreate = {
+  tags: ["Correction"],
+  description: "Create a correction",
+  body: CorrectionCreateSchema,
+  response: { 201: CorrectionResponseSchema },
 };
 
 const CorrectionUpdateSchema = {
@@ -15,16 +28,37 @@ const CorrectionUpdateSchema = {
   properties: { id: { type: "string" }, ...CorrectionSchema },
 };
 
+export const validationSchemaUpdate = {
+  tags: ["Correction"],
+  description: "Update a correction",
+  body: CorrectionUpdateSchema,
+  response: { 200: CorrectionResponseSchema },
+};
+
 const CorrectionDeleteSchema = {
   type: "object",
   required: ["correctionId"],
   properties: { correctionId: { type: "string" } },
 };
 
+export const validationSchemaDelete = {
+  tags: ["Correction"],
+  description: "Delete a correction",
+  body: CorrectionDeleteSchema,
+  response: { 200: { type: "boolean" } },
+};
+
 const CorrectionDeleteByFinancialPartIdSchema = {
   type: "object",
   required: ["financialPartId"],
   properties: { financialPartId: { type: "string" } },
+};
+
+export const validationSchemaDeleteByFinancialPartId = {
+  tags: ["Correction"],
+  description: "Delete corrections by financial part id",
+  body: CorrectionDeleteByFinancialPartIdSchema,
+  response: { 200: { type: "boolean" } },
 };
 
 const CorrectionSearchSchema = {
@@ -37,40 +71,6 @@ const CorrectionSearchSchema = {
       items: { type: "string" },
     },
   },
-};
-
-const CorrectionResponseSchema = {
-  type: "object",
-  required: ["id", "name", "amount"],
-  properties: { id: { type: "string" }, ...CorrectionSchema },
-};
-
-export const validationSchemaCreate = {
-  tags: ["Correction"],
-  description: "Create a correction",
-  body: CorrectionCreateSchema,
-  response: { 201: CorrectionResponseSchema },
-};
-
-export const validationSchemaUpdate = {
-  tags: ["Correction"],
-  description: "Update a correction",
-  body: CorrectionUpdateSchema,
-  response: { 200: CorrectionResponseSchema },
-};
-
-export const validationSchemaDelete = {
-  tags: ["Correction"],
-  description: "Delete a correction",
-  body: CorrectionDeleteSchema,
-  response: { 200: { type: "boolean" } },
-};
-
-export const validationSchemaDeleteByFinancialPartId = {
-  tags: ["Correction"],
-  description: "Delete corrections by financial part id",
-  body: CorrectionDeleteByFinancialPartIdSchema,
-  response: { 200: { type: "boolean" } },
 };
 
 export const validationSchemaSearch = {

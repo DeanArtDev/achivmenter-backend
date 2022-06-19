ccf02f45054f14a4f5fcd0ad9ded56ca1043b70c
Dmitriy Korobchenko <deankorobchenko@scrile.com>
2022-06-19 15:13:09 +0700

    Revert "temp changes"
    This reverts commit ad449fe1c86c4f689b47c28fef348bb632d2fbff.

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -10,9 +10,7 @@ export default class User {
   }
 
   public async encodePassword(password: string, salt: number): Promise<void> {
-    const result = await hash(password, salt);
-    console.log("encodePassword", password, salt, "result: ", result);
-    this._password = result;
+    this._password = await hash(password, salt);
   }
 
   public async comparePassword(password: string): Promise<boolean> {
